d9de510b6fb6cebe2b56eaf92310b74983c46f38
thangbd200230 <thangbd19964@gmail.com>
2023-03-13 00:57:46 +0700

    newosojdf

---
+++ src/controller/home_page.js
@@ -0,0 +1,22 @@
+window.homeController = function ($scope) {
+  $scope.sinhVien = [
+    {
+      id: "ID",
+      ten: "Ten",
+      tuoi: "Tuoi",
+      gioiTinh: "Gioi tinh",
+    },
+    {
+      id: "1",
+      ten: "Thang",
+      tuoi: "18",
+      gioiTinh: "Nam",
+    },
+    {
+      id: "2",
+      ten: "Thang2",
+      tuoi: "20",
+      gioiTinh: "Nữ",
+    },
+  ];
+};
---
+++ src/router.js
@@ -0,0 +1,24 @@
+var myApp = angular.module("myModule", ["ngRoute"]);
+// chuyen trang
+myApp.config(function ($routeProvider, $locationProvider) {
+  // xoa khoang trang trinh duyet
+  $locationProvider.hashPrefix("");
+  // $routeProvider : chuyen trang
+  $routeProvider
+    .when("/facebook", {
+      // lam gi thi lam
+      // noi dung cua trang chu
+      templateUrl: "/src/pages/facebook.html",
+      controller: homeController,
+    })
+    .when("/gioithieu", {
+      // lam gi thi lam
+      // noi dung cua trang chu
+      //   template: "<h1>Noi dung trang contact</h1>",
+      //Truyen file HTML
+      templateUrl: "pages/gioiThieu.html",
+    })
+    .otherwise({
+      redirectTo: "/trangchu",
+    });
+});
